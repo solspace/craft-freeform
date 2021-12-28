941aea1f84530c9001a4752e10763968637cb45c
Gustavs <gustavs.gutmanis@gmail.com>
2021-12-28 12:07:42 +0200

    chore: updating PHP CS fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,14 +9,15 @@ return (new PhpCsFixer\Config())
         '@Symfony:risky' => true,
         '@PHPUnit75Migration:risky' => true,
         '@PhpCsFixer' => true,
-        'protected_to_private' => false,
-        'combine_nested_dirname' => true,
-        'array_syntax' => ['syntax' => 'short'],
         'array_indentation' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'combine_nested_dirname' => true,
         'linebreak_after_opening_tag' => true,
         'list_syntax' => ['syntax' => 'long'],
+        'protected_to_private' => false,
         'single_trait_insert_per_statement' => true,
         'ternary_to_null_coalescing' => true,
+        'visibility_required' => ['elements' => ['property', 'method']],
     ])
     ->setRiskyAllowed(true)
     ->setFinder($finder);

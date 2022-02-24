dceadca126cb66a11238b06afd2e7001db002071
Gustavs <gustavs.gutmanis@gmail.com>
2022-02-24 14:59:42 +0200

    fix: fixed success banner not showing on "reload form with success message" for ajax forms

--- packages/scripts/src/components/front-end/plugin/freeform.js
+++ packages/scripts/src/components/front-end/plugin/freeform.js
@@ -18,7 +18,11 @@ import GoogleTagManager from '@lib/plugin/handlers/form/google-tag-manager';
 import SaveFormHandler from '@lib/plugin/handlers/form/save-form';
 import { isSafari } from '@lib/plugin/helpers/browser-check';
 import { addClass, getClassArray, removeClass } from '@lib/plugin/helpers/elements';
-import { SUCCESS_BEHAVIOUR_NO_EFFECT, SUCCESS_BEHAVIOUR_REDIRECT_RETURN_URL } from '@lib/plugin/constants/form';
+import {
+  SUCCESS_BEHAVIOUR_NO_EFFECT,
+  SUCCESS_BEHAVIOUR_REDIRECT_RETURN_URL,
+  SUCCESS_BEHAVIOUR_RELOAD,
+} from '@lib/plugin/constants/form';
 
 export default class Freeform {
   static _BACK_BUTTON_NAME = 'form_previous_page_button';
@@ -685,7 +689,7 @@ export default class Freeform {
                 this._dispatchEvent(EventTypes.EVENT_ON_RESET);
               }
 
-              if (response.onSuccess === SUCCESS_BEHAVIOUR_NO_EFFECT) {
+              if ([SUCCESS_BEHAVIOUR_NO_EFFECT, SUCCESS_BEHAVIOUR_RELOAD].includes(response.onSuccess)) {
                 this._renderSuccessBanner();
               }
             }
--- packages/scripts/src/lib/plugin/constants/form.js
+++ packages/scripts/src/lib/plugin/constants/form.js
@@ -1,3 +1,4 @@
 export const SUCCESS_BEHAVIOUR_NO_EFFECT = 'no-effect';
+export const SUCCESS_BEHAVIOUR_RELOAD = 'reload';
 export const SUCCESS_BEHAVIOUR_REDIRECT_RETURN_URL = 'redirect-return-url';
 export const SUCCESS_BEHAVIOUR_LOAD_SUCCESS_TEMPLATE = 'load-success-template';

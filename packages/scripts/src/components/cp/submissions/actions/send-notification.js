38e09a001f85ae2bf3f430c567ed6234923410c1
Gustavs <gustavs.gutmanis@gmail.com>
2022-03-01 18:22:09 +0200

    fix: fixed cancel button on send notifications

--- packages/scripts/src/components/cp/submissions/actions/send-notification.js
+++ packages/scripts/src/components/cp/submissions/actions/send-notification.js
@@ -2,66 +2,52 @@
 
 window.freeform_notify = (ids) => {
   const content = $('<div id="export-modal-wrapper" class="modal fitted">');
-  const exportWrapper = $('#export-modal-wrapper');
-
-  const modal = new Garnish.Modal(content, {
-    onHide: () => {
-      setTimeout(() => {
-        $('#export-modal-wrapper').remove();
-        $('.modal-shade').remove();
-      }, 10);
-    },
-    onShow: () => {
-      const modal = $('#export-modal-wrapper');
-
-      $('.btn.cancel', modal).on({
-        click: () => {
-          modal.data('modal').hide();
-        },
-      });
-    },
-  });
-
-  exportWrapper.data('modal', modal);
 
   let form;
 
   $.ajax({
     url: Craft.getCpUrl('freeform/notifications/send-notification-dialogue'),
-    async: false,
-    cache: false,
     dataType: 'html',
     success: (response) => {
       content.html(response);
+
+      new Garnish.Modal(content);
+
       form = content.find('form').get(0);
+
+      $('.btn.cancel', content).on({
+        click: () => {
+          content.remove();
+          $('.modal-shade').remove();
+        },
+      });
+
+      form.addEventListener('submit', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const data = {
+          template: form.template.value,
+          emails: form.emails.value,
+          submissionIds: ids,
+        };
+
+        $.ajax({
+          url: Craft.getCpUrl('freeform/notifications/send-notification'),
+          type: 'post',
+          data,
+          success: () => {
+            content.remove();
+            $('.modal-shade').remove();
+          },
+          error: (error) => {
+            console.log(error);
+            alert(error.responseText);
+          },
+        });
+
+        return false;
+      });
     },
-  });
-
-  form.addEventListener('submit', (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    const data = {
-      template: form.template.value,
-      emails: form.emails.value,
-      submissionIds: ids,
-    };
-
-    $.ajax({
-      url: Craft.getCpUrl('freeform/notifications/send-notification'),
-      type: 'post',
-      data,
-      success: () => {
-        $('#export-modal-wrapper').remove();
-        $('.modal-shade').remove();
-        modal.data('modal').hide();
-      },
-      error: (error) => {
-        console.log(error);
-        alert(error.responseText);
-      },
-    });
-
-    return false;
   });
 };

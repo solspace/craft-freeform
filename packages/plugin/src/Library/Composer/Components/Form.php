4b8b216ca2af595d6f853e2647dfe4853c026bca
Kelsey Martens <kmartens@Kelseys-MacBook-Air.local>
2022-04-14 15:12:44 -0500

    Updating copyright year

--- packages/plugin/src/Library/Composer/Components/Form.php
+++ packages/plugin/src/Library/Composer/Components/Form.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * Freeform for Craft.
+ * Freeform for Craft CMS
  *
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2021, Solspace, Inc.
- *
- * @see           https:   //solspace.com/craft/freeform
- *
- * @license       https:   //solspace.com/software/license-agreement
+ * @copyright     Copyright (c) 2008-2022, Solspace, Inc.
+ * @see           https://docs.solspace.com/craft/freeform
+ * @license       https://docs.solspace.com/license-agreement
  */
 
 namespace Solspace\Freeform\Library\Composer\Components;

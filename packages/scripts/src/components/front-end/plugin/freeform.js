81ca096cc5e53dbc75d0e599be5916168b174038
Gustavs Gūtmanis <gustavs@solspace.com>
2022-08-24 12:27:08 +0300

    fix: fixed multiple instances of the same form being added when moving the form instance around the DOM

--- packages/scripts/src/components/front-end/plugin/freeform.js
+++ packages/scripts/src/components/front-end/plugin/freeform.js
@@ -95,6 +95,10 @@ export default class Freeform {
    * @param {Element} form
    */
   constructor(form) {
+    if (Freeform.instances.get(form)) {
+      return Freeform.instances.get(form);
+    }
+
     this.id = form.dataset.id;
     this.form = form;
 

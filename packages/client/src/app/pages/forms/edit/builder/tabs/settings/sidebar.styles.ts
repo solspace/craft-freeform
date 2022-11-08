7fc10d08dfc159fbe26e52579a87878886dec26d
Sean Delaney <sean@solspace.com>
2022-11-08 15:27:10 +0000

    SFT-54 / SFT-55: Form Settings

--- packages/client/src/app/pages/forms/edit/builder/tabs/settings/sidebar.styles.ts
+++ packages/client/src/app/pages/forms/edit/builder/tabs/settings/sidebar.styles.ts
@@ -8,16 +8,16 @@ export const SidebarWrapper = styled.div`
   flex-directions: row;
   align-items: flex-start;
   justify-content: flex-start;
+`;
 
-  h2 {
-    margin: 0;
-    width: 100%;
-    height: auto;
-    display: flex;
-    padding: 20px;
-    font-weight: 700;
-    line-height: 1.2;
-    align-items: center;
-    justify-content: flex-start;
-  }
+export const SidebarHeading = styled.h2`
+  margin: 0;
+  width: 100%;
+  height: auto;
+  display: flex;
+  padding: 20px;
+  font-weight: 700;
+  line-height: 1.2;
+  align-items: center;
+  justify-content: flex-start;
 `;
--- packages/client/src/app/pages/forms/edit/builder/tabs/settings/sidebar.tsx
+++ packages/client/src/app/pages/forms/edit/builder/tabs/settings/sidebar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { SidebarWrapper } from './sidebar.styles';
+import { SidebarHeading, SidebarWrapper } from './sidebar.styles';
 
 export const Sidebar: React.FC = () => (
   <SidebarWrapper>
-    <h2>Settings Sidebar</h2>
+    <SidebarHeading>Settings Sidebar</SidebarHeading>
   </SidebarWrapper>
 );

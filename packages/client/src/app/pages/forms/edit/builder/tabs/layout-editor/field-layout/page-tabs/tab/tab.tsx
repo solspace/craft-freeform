13f3d266b181129d37ec31958ee60a9d5c1309ca
Sean Delaney <sean@solspace.com>
2023-03-03 15:02:21 +0000

    [SFT-178] Rename Page by double-clicking it

--- packages/client/src/app/pages/forms/edit/builder/tabs/layout-editor/field-layout/page-tabs/tab/tab.tsx
+++ packages/client/src/app/pages/forms/edit/builder/tabs/layout-editor/field-layout/page-tabs/tab/tab.tsx
@@ -1,9 +1,11 @@
-import type { MutableRefObject } from 'react';
-import React, { useRef } from 'react';
+import type { KeyboardEvent, MutableRefObject } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import type { Page } from '@editor/builder/types/layout';
 import { useAppDispatch } from '@editor/store';
 import { selectCurrentPage, setPage } from '@editor/store/slices/context';
+import { updateLabel } from '@editor/store/slices/pages';
+import { useClickOutside } from '@ff-client/hooks/use-click-outside';
 import classes from '@ff-client/utils/classes';
 
 import { useDragContext } from '../../../drag.context';
@@ -11,7 +13,7 @@ import { useDragContext } from '../../../drag.context';
 import { useTabDrag } from './tab.drag';
 import { useTabDragAnimation } from './tab.drag-animation';
 import { useTabDrop, useTabPageDrop } from './tab.drop';
-import { PageTab, TabDrop, TabWrapper } from './tab.styles';
+import { Input, PageTab, TabDrop, TabWrapper } from './tab.styles';
 
 type Props = {
   page: Page;
@@ -25,6 +27,8 @@ export const Tab: React.FC<Props> = ({ page, index }) => {
 
   const wrapperRef = useRef<HTMLDivElement>(null);
 
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
   const { canDrop, ref: dropRef } = useTabDrop(uid, page);
   const { isDragging, ref: dragRef } = useTabDrag(index, page);
 
@@ -34,9 +38,9 @@ export const Tab: React.FC<Props> = ({ page, index }) => {
     index
   );
 
-  const connectedRef = dropRef(
+  const connectedRef = (dropRef(
     dragRef(wrapperRef)
-  ) as unknown as MutableRefObject<HTMLDivElement>;
+  ) as unknown) as MutableRefObject<HTMLDivElement>;
 
   const style = useTabDragAnimation(
     wrapperRef,
@@ -45,21 +49,56 @@ export const Tab: React.FC<Props> = ({ page, index }) => {
     isDragging
   );
 
+  const handleKeyboardEvent = (
+    event: KeyboardEvent<HTMLInputElement>
+  ): void => {
+    if (event.key === 'Enter') {
+      dispatch(
+        updateLabel({
+          uid: page.uid,
+          label: (event.target as HTMLInputElement).value || page.label,
+        })
+      );
+
+      setIsEditing(false);
+    }
+  };
+
+  const ref = useClickOutside<HTMLDivElement>(
+    (): void => setIsEditing(false),
+    isEditing
+  );
+
   return (
     <TabWrapper ref={connectedRef}>
       {(!!dragType || isDragging) && <TabDrop ref={dropPageRef} />}
       <PageTab
+        ref={ref}
         className={classes(
           uid === page.uid && 'active',
           canDrop && 'can-drop',
+          isEditing && 'is-editing',
           isDragging && 'is-dragging'
         )}
         style={style}
         onClick={(): void => {
+          setIsEditing(false);
           dispatch(setPage(page.uid));
         }}
+        onDoubleClick={(): void => setIsEditing(true)}
       >
-        {page.label}
+        {isEditing ? (
+          <Input
+            type="text"
+            autoFocus={true}
+            className="text small"
+            placeholder={page.label}
+            defaultValue={page.label}
+            onKeyUp={handleKeyboardEvent}
+          />
+        ) : (
+          page.label
+        )}
       </PageTab>
     </TabWrapper>
   );

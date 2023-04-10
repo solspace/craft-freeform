1bcb70f2ca4f3d9ac58eead91a072cbb4707c574
kjmartens <kelsey@solspace.com>
2023-04-10 02:15:59 -0500

    WIP Adjustments to sample templates and new demo templates area
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Adjustments to sample templates and new demo templates area
    
    WIP Cleanup of new demo templates
    
    WIP Cleanup of new demo templates
    
    WIP Cleanup of new demo templates

--- packages/plugin/src/codepack/routes.php
+++ packages/plugin/src/codepack/routes.php
@@ -3,7 +3,7 @@
  * Freeform for Craft CMS.
  *
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2022, Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2023, Solspace, Inc.
  *
  * @see           https://docs.solspace.com/craft/freeform
  *
@@ -11,21 +11,15 @@
  */
 
 return [
-    'demo/bootstrap/<slug:[^\/]+>/submissions/<id\d+>/success' => ['template' => 'demo/bootstrap/view_submission'],
-    'demo/bootstrap/<slug:[^\/]+>/submissions/<id\d+>' => ['template' => 'demo/bootstrap/view_submission'],
-    'demo/bootstrap/<slug:[^\/]+>/submissions/edit/<token\w+>' => ['template' => 'demo/bootstrap/edit_submission'],
-    'demo/bootstrap/<slug:[^\/]+>/submissions/delete/<token\w+>' => ['template' => 'demo/bootstrap/delete_submission'],
-    'demo/bootstrap/<slug:[^\/]+>/submissions' => ['template' => 'demo/bootstrap/submissions'],
-    'demo/bootstrap/<slug:[^\/]+>' => ['template' => 'demo/bootstrap/form'],
-    'demo/bootstrap/<slug:[^\/]+>/success' => ['template' => 'demo/bootstrap/form'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/submissions/<id\d+>/success' => ['template' => 'demo/bootstrap-dark/view_submission'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/submissions/<id\d+>' => ['template' => 'demo/bootstrap-dark/view_submission'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/submissions/edit/<token\w+>' => ['template' => 'demo/bootstrap-dark/edit_submission'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/submissions/delete/<token\w+>' => ['template' => 'demo/bootstrap-dark/delete_submission'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/submissions' => ['template' => 'demo/bootstrap-dark/submissions'],
-    'demo/bootstrap-dark/<slug:[^\/]+>' => ['template' => 'demo/bootstrap-dark/form'],
-    'demo/bootstrap-dark/<slug:[^\/]+>/success' => ['template' => 'demo/bootstrap-dark/form'],
-    'demo/custom/<slug:[^\/]+>' => ['template' => 'demo/custom/form'],
-    'demo/custom/<slug:[^\/]+>/success' => ['template' => 'demo/custom/form'],
+    'demo/templates/<*:[^\/]+>' => ['template' => 'demo/templates'],
+    'demo/templates/<*:[^\/]+>/<*:[^\/]+>' => ['template' => 'demo/templates/form'],
+    'demo/templates/<*:[^\/]+>/<*:[^\/]+>/edit/<*:[^\/]+>' => ['template' => 'demo/templates/form'],
+    'demo/submissions/<*:[^\/]+>' => ['template' => 'demo/submissions'],
+    'demo/submissions/<*:[^\/]+>/spam' => ['template' => 'demo/submissions'],
+    'demo/submissions/<*:[^\/]+>/<*:[^\/]+>' => ['template' => 'demo/submissions/view'],
+    'demo/submissions/<*:[^\/]+>/<*:[^\/]+>/spam' => ['template' => 'demo/submissions/view'],
+    'demo/submissions/delete/<*:[^\/]+>/<*:[^\/]+>' => ['template' => 'demo/submissions/delete'],
+    'demo/custom' => ['template' => 'demo/custom'],
+    'demo/custom/<*:[^\/]+>' => ['template' => 'demo/custom/form'],
     'demo/extras/suppress-edit-submissions/edit/<token\w+>' => ['template' => 'demo/extras/suppress-edit-submissions'],
 ];

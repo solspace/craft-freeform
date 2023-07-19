6b3f87f7c5b840dfd8541f0a7ec37d8b0195576d
Gustavs Gūtmanis <gustavs@solspace.com>
2023-07-19 17:58:08 +0300

    feat(SFT-458): added field type change dropdown

--- packages/client/src/app/pages/forms/edit/store/thunks/fields.ts
+++ packages/client/src/app/pages/forms/edit/store/thunks/fields.ts
@@ -1,9 +1,13 @@
 import type { Row } from '@editor/builder/types/layout';
 import { CellType } from '@editor/builder/types/layout';
 import type { AppThunk } from '@editor/store';
+import type { Field } from '@editor/store/slices/fields';
 import { fieldActions } from '@editor/store/slices/fields';
 import { cellActions } from '@editor/store/slices/layout/cells';
-import type { FieldType } from '@ff-client/types/fields';
+import type {
+  FieldType,
+  PropertyValueCollection,
+} from '@ff-client/types/fields';
 import { v4 } from 'uuid';
 
 import { contextSelectors } from '../slices/context/context.selectors';
@@ -62,6 +66,32 @@ export const addNewFieldToExistingRow =
         targetUid: fieldUid,
         uid: cellUid,
         order,
+      })
+    );
+  };
+
+export const changeFieldType =
+  (field: Field, type: FieldType): AppThunk =>
+  (dispatch) => {
+    const { uid } = field;
+
+    const properties: PropertyValueCollection = {};
+
+    type.properties.forEach((property) => {
+      const targetProperty = field.properties[property.handle];
+
+      if (targetProperty) {
+        properties[property.handle] = targetProperty;
+      } else {
+        properties[property.handle] = property.value;
+      }
+    });
+
+    dispatch(
+      fieldActions.batchEdit({
+        uid,
+        typeClass: type.typeClass,
+        properties,
       })
     );
   };

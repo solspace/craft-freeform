22ea478e759947eaaa9ff1b7608850a9b219fa8d
Sean Delaney <sean@solspace.com>
2023-07-31 11:43:17 +0100

    [SFT-487] - Form Builder Settings - Fixed Success Template Options

--- packages/plugin/src/Form/Settings/Implementations/Options/SuccessTemplateOptions.php
+++ packages/plugin/src/Form/Settings/Implementations/Options/SuccessTemplateOptions.php
@@ -5,11 +5,23 @@ namespace Solspace\Freeform\Form\Settings\Implementations\Options;
 use Solspace\Freeform\Attributes\Property\Implementations\Options\OptionCollection;
 use Solspace\Freeform\Attributes\Property\Implementations\Options\OptionsGeneratorInterface;
 use Solspace\Freeform\Attributes\Property\Property;
+use Solspace\Freeform\Services\SettingsService;
 
 class SuccessTemplateOptions implements OptionsGeneratorInterface
 {
+    public function __construct(private SettingsService $settingsService)
+    {
+    }
+
     public function fetchOptions(Property $property): OptionCollection
     {
-        return new OptionCollection();
+        $options = new OptionCollection();
+
+        $templates = $this->settingsService->getSuccessTemplates();
+        foreach ($templates as $template) {
+            $options->add($template->getFileName(), $template->getName());
+        }
+
+        return $options;
     }
 }

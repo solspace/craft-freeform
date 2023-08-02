374cd398dd03b4dd61c847f5bf2d9ab796179ea7
Gustavs Gūtmanis <gustavs@solspace.com>
2023-08-02 17:32:17 +0300

    fix(SFT-510): fixing going back to forms list and back to form causing stale data to be displayed

--- packages/client/src/hooks/use-freeform-navigation.ts
+++ packages/client/src/hooks/use-freeform-navigation.ts
@@ -1,8 +1,12 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { QKForms } from '@ff-client/queries/forms';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const useFreeformNavigation = (): void => {
+  const { formId } = useParams();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     const link = document.querySelector(
@@ -11,6 +15,10 @@ export const useFreeformNavigation = (): void => {
 
     const onClick = (event: MouseEvent): boolean => {
       event.preventDefault();
+
+      if (formId) {
+        queryClient.invalidateQueries(QKForms.single(Number(formId)));
+      }
 
       navigate('/forms');
 

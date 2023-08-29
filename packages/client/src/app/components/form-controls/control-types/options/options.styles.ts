9eb0659613a3dd58d355b4b9417e106a58eca0d2
Gustavs Gūtmanis <gustavs@solspace.com>
2023-08-29 14:18:18 +0300

    fix(SFT-575): improving option source buttons

---
+++ packages/client/src/app/components/form-controls/control-types/options/options.styles.ts
@@ -0,0 +1,44 @@
+import {
+  borderRadius,
+  colors,
+  shadows,
+  spacings,
+} from '@ff-client/styles/variables';
+import styled from 'styled-components';
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  display: block;
+  flex: 1;
+
+  padding: ${spacings.xs} ${spacings.md};
+
+  background-color: ${colors.gray100};
+  box-shadow: ${shadows.right};
+  box-sizing: border-box;
+
+  &.active {
+    color: ${colors.white};
+    background-color: ${colors.gray500};
+  }
+
+  &:first-child {
+    border-top-left-radius: ${borderRadius.lg};
+    border-bottom-left-radius: ${borderRadius.lg};
+  }
+
+  &:last-child {
+    border-top-right-radius: ${borderRadius.lg};
+    border-bottom-right-radius: ${borderRadius.lg};
+
+    box-shadow: none;
+  }
+`;

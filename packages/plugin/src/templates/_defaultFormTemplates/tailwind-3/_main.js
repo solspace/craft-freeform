417bc3dfa83b3634cc1c6751091efb34a67a4412
kjmartens <kelsey@solspace.com>
2023-09-01 14:42:07 -0500

    Adjustments to all formatting templates using new changes

--- packages/plugin/src/templates/_defaultFormTemplates/tailwind-3/_main.js
+++ packages/plugin/src/templates/_defaultFormTemplates/tailwind-3/_main.js
@@ -3,10 +3,10 @@ if (form) {
     // Styling for AJAX responses
     form.addEventListener("freeform-ready", function (event) {
         var freeform = event.target.freeform;
-        freeform.setOption("errorClassBanner", ["bg-red-100", "border", "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "relative", "mb-4"]);
+        freeform.setOption("errorClassBanner", ["bg-red-100", "border", "border-red-400", "font-bold", "text-red-700", "px-4", "py-3", "rounded", "relative", "mb-4"]);
         freeform.setOption("errorClassList", ["errors", "text-red-500", "text-sm", "italic"]);
         freeform.setOption("errorClassField", ["border-red-500"]);
-        freeform.setOption("successClassBanner", ["bg-green-100", "border", "border-green-500", "text-green-700", "px-4", "py-3", "rounded", "relative", "mb-4"]);
+        freeform.setOption("successClassBanner", ["bg-green-100", "border", "border-green-500", "font-bold", "text-green-700", "px-4", "py-3", "rounded", "relative", "mb-4"]);
     })
     // Styling for Stripe Payments field
     form.addEventListener("freeform-stripe-styling", function (event) {

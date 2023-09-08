44f3554834af16806a9db7a0f0c96f6ce4b69c98
Gustavs Gūtmanis <gustavs@solspace.com>
2023-09-08 14:36:15 +0300

    fix(SFT-624): fixing notification template selector height

--- packages/client/src/app/components/form-controls/control-types/namespaced/notifications/notification-template/notification-template.animations.ts
+++ packages/client/src/app/components/form-controls/control-types/namespaced/notifications/notification-template/notification-template.animations.ts
@@ -2,10 +2,18 @@ import type { SpringValues } from 'react-spring';
 import { useSpring } from 'react-spring';
 
 export const useEditorAnimations = (
-  open: boolean
+  open: boolean,
+  itemCount: number
 ): SpringValues<{ height: number }> => {
+  let height = 210;
+  if (itemCount > 6) {
+    height = 360;
+  } else if (itemCount > 3) {
+    height = 280;
+  }
+
   return useSpring({
-    height: open ? 400 : 36,
+    height: open ? height : 36,
     config: {
       tension: 500,
       friction: open ? 26 : 40,

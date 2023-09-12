450e8840c2720d82d2e0936b5982d81aabbcfe5d
Gustavs Gūtmanis <gustavs@solspace.com>
2023-09-12 17:46:35 +0300

    feat(SFT-647): adding submit button layouts

--- packages/client/src/app/components/form-controls/control-types/namespaced/page-buttons/button-layout/layout.tsx
+++ packages/client/src/app/components/form-controls/control-types/namespaced/page-buttons/button-layout/layout.tsx
@@ -1,40 +1,77 @@
+import type { ReactNode } from 'react';
 import React from 'react';
 import { Control } from '@components/form-controls/control';
 import type { ControlType } from '@components/form-controls/types';
+import type { Page } from '@editor/builder/types/layout';
 import type { PageButtonsLayoutProperty } from '@ff-client/types/properties';
-import translate from '@ff-client/utils/translations';
+import classes from '@ff-client/utils/classes';
 
-const PageButtonLayout: React.FC<ControlType<PageButtonsLayoutProperty>> = ({
-  value,
-  property,
-  errors,
-  updateValue,
-}) => {
+import BackIcon from './icons/back.svg';
+import SaveIcon from './icons/save.svg';
+import SubmitIcon from './icons/submit.svg';
+import {
+  Button,
+  ButtonGroup,
+  ButtonLayoutWrapper,
+  LayoutBlock,
+} from './layout.styles';
+
+const layouts = [
+  'back|submit|save',
+  'save|back|submit',
+  ' back|submit|save',
+  ' save|back|submit',
+  ' save|back|submit ',
+  ' back|submit|save ',
+  'back|submit save',
+  'save back|submit',
+];
+
+const icons: Record<string, ReactNode> = {
+  save: <SaveIcon />,
+  back: <BackIcon />,
+  submit: <SubmitIcon />,
+};
+
+const PageButtonLayout: React.FC<
+  ControlType<PageButtonsLayoutProperty, Page>
+> = ({ value, property, errors, updateValue, context }) => {
+  const buttonState: Record<string, boolean> = {
+    save: context?.buttons?.save?.enabled,
+    back: context?.buttons?.back?.enabled,
+    submit: context?.buttons?.submit?.enabled,
+  };
+
   return (
     <Control property={property} errors={errors}>
-      <div className="select fullwidth">
-        <select
-          value={value}
-          onChange={(event) => updateValue(event.target.value)}
-        >
-          <option value="back|submit|save">{translate('Left')}</option>
-          <option value="save|back|submit">
-            {translate('Left (save on left)')}
-          </option>
-          <option value=" back|submit|save">{translate('Right')}</option>
-          <option value=" save|back|submit">
-            {translate('Right (save on left)')}
-          </option>
-          <option value=" save|back|submit ">
-            {translate('Center (save on left)')}
-          </option>
-          <option value=" back|submit|save ">
-            {translate('Center (save on right)')}
-          </option>
-          <option value="back|submit save">{translate('Save on right')}</option>
-          <option value="save back|submit">{translate('Save on left')}</option>
-        </select>
-      </div>
+      <ButtonLayoutWrapper>
+        {layouts.map((item, idx) => (
+          <LayoutBlock
+            key={idx}
+            onClick={() => updateValue(item)}
+            className={classes(value === item && 'active')}
+          >
+            {item.split(' ').map((group, groupIdx) => (
+              <ButtonGroup key={groupIdx}>
+                {group
+                  .split('|')
+                  .filter(Boolean)
+                  .map((button, buttonIdx) => (
+                    <Button
+                      className={classes(
+                        button,
+                        buttonState?.[button] && 'enabled'
+                      )}
+                      key={buttonIdx}
+                    >
+                      {icons[button]}
+                    </Button>
+                  ))}
+              </ButtonGroup>
+            ))}
+          </LayoutBlock>
+        ))}
+      </ButtonLayoutWrapper>
     </Control>
   );
 };

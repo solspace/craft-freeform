cfd4aaa4fa4343a3c080456b1c13a6b20b1078e6
kjmartens <kelsey@solspace.com>
2023-09-29 20:38:42 -0500

    Stuffing in v4 announcement migrations with new adminOnly: true

---
+++ packages/plugin/src/migrations/m230613_010101_announcement_4_1_0.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Solspace\Freeform\migrations;
+
+use craft\db\Migration;
+use craft\i18n\Translation;
+
+/**
+ * m230613_010101_announcement_4_1_0 migration.
+ */
+class m230613_010101_announcement_4_1_0 extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): bool
+    {
+        \Craft::$app->announcements->push(
+            Translation::prep('freeform', 'Improved GraphQL / Headless support!', []),
+            Translation::prep('freeform', 'Added support for [GraphQL Mutations]({url1}) and significantly improved support for [headless implementations]({url2}).', [
+                'url1' => 'https://docs.solspace.com/craft/freeform/v4/headless/graphql/',
+                'url2' => 'https://docs.solspace.com/craft/freeform/v4/headless/',
+            ]),
+            'freeform',
+            true
+        );
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): bool
+    {
+        echo "m230613_010101_announcement_4_1_0 cannot be reverted.\n";
+
+        return false;
+    }
+}

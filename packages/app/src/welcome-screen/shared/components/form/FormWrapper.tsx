e8e0e6f49e918cd782845f676c0afef740ce0491
Gustavs Gūtmanis <gustavs@solspace.com>
2023-10-17 12:12:41 +0300

    feat(SFT-715): removing legacy react app

--- packages/app/src/welcome-screen/shared/components/form/FormWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import { Wrapper } from './FormWrapper.styles';
-
-const FormWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
-};
-
-export default FormWrapper;

988fd362ef070bb283c9ca5280a9b369114cb882
Sean Delaney <sean@solspace.com>
2024-01-05 12:54:17 +0000

    [SFT-145] - Migrations - Removed duplicate drop column

--- packages/plugin/src/migrations/m230101_100010_FF4to5_MigrateForms.php
+++ packages/plugin/src/migrations/m230101_100010_FF4to5_MigrateForms.php
@@ -96,8 +96,6 @@ class m230101_100010_FF4to5_MigrateForms extends Migration
                 ['metadata' => $metadata],
                 ['id' => $id],
             );
-
-            $this->dropColumn('{{%freeform_forms}}', 'layoutJson');
         }
 
         return true;

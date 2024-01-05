650869118ae1adfa25b04b035358562e8479f7cb
Aaron Linley <linley.aaron@gmail.com>
2024-01-05 17:30:18 +0000

    style: run prettier

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -16,7 +16,7 @@ const config = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath("@storybook/addon-mdx-gfm")
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),

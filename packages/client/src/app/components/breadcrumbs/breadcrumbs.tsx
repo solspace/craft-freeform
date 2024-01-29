0900181ab61f9d632836a831616f5cd33f3a25a6
Gustavs Gūtmanis <gustavs@solspace.com>
2024-01-29 11:51:56 +0200

    fix(SFT-853): fixing breadcrumb issue on save

--- packages/client/src/app/components/breadcrumbs/breadcrumbs.tsx
+++ packages/client/src/app/components/breadcrumbs/breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import type React from 'react';
+import { useEffect } from 'react';
 
 import { useBreadcrumbs } from './breadcrumbs.context';
 import type { Breadcrumb as BreadcrumbType } from './breadcrumbs.types';
@@ -6,7 +7,15 @@ import type { Breadcrumb as BreadcrumbType } from './breadcrumbs.types';
 type Props = BreadcrumbType;
 
 export const Breadcrumb: React.FC<Props> = (crumb) => {
-  useBreadcrumbs(crumb);
+  const { pushCrumb, popCrumb } = useBreadcrumbs();
+
+  useEffect(() => {
+    pushCrumb(crumb);
+
+    return () => {
+      popCrumb();
+    };
+  }, []);
 
   return null;
 };

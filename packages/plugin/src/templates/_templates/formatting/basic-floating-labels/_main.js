a95b874aa6045020555579b67f1645f2535c5453
kjmartens <kelsey@solspace.com>
2024-02-26 23:51:14 -0600

    Updated all sample formatting templates to include complete Stripe appearance API customization

--- packages/plugin/src/templates/_templates/formatting/basic-floating-labels/_main.js
+++ packages/plugin/src/templates/_templates/formatting/basic-floating-labels/_main.js
@@ -1,10 +1,43 @@
 var form = document.querySelector('[data-id="{{ form.anchor }}"]');
 if (form) {
-    form.addEventListener("freeform-stripe-styling", function (event) {
-        event.detail.base = {
-            fontSize: "16px",
-            color: "#464747",
-            fontFamily: "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,\"Helvetica Neue\",Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\",\"Noto Color Emoji\"",
-        }
-    })
+    form.addEventListener("freeform-stripe-appearance", function (event) {
+        event.elementOptions.appearance = Object.assign(
+            event.elementOptions.appearance,
+            {
+                variables: {
+                    colorPrimary: "#0d6efd",
+                    fontFamily: "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,\"Helvetica Neue\",Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\",\"Noto Color Emoji\"",
+                    fontSizeBase: "16px",
+                    spacingUnit: "0.2em",
+                    tabSpacing: "10px",
+                    gridColumnSpacing: "20px",
+                    gridRowSpacing: "20px",
+                    colorText: "#212529",
+                    colorBackground: "#ffffff",
+                    colorDanger: "#dc3545",
+                    borderRadius: "5px",
+                },
+                rules: {
+                    '.Tab, .Input': {
+                        border: '1px solid #cbced0',
+                        boxShadow: 'none',
+                    },
+                    '.Tab:focus, .Input:focus': {
+                        border: '1px solid #0b5ed7',
+                        boxShadow: 'none',
+                        outline: '0',
+                        transition: 'border-color .15s ease-in-out',
+                    },
+                    '.Label--resting': {
+                        fontSize: '16px',
+                        fontWeight: '400',
+                    },
+                    '.Label--floating': {
+                        fontSize: '13px',
+                        fontWeight: 'normal',
+                    },
+                },
+            }
+        );
+    });
 }
--- packages/plugin/src/templates/_templates/formatting/foundation-6/_main.js
+++ packages/plugin/src/templates/_templates/formatting/foundation-6/_main.js
@@ -9,10 +9,14 @@ if (form) {
         freeform.setOption("successClassBanner", ["callout", "success"]);
     })
     // Styling for Stripe Payments field
-    form.addEventListener("freeform-stripe-styling", function (event) {
-        event.detail.base = {
-            fontSize: "16px",
-            fontFamily: "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,\"Helvetica Neue\",Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\",\"Noto Color Emoji\"",
-        }
-    })
+    form.addEventListener("freeform-stripe-appearance", function (event) {
+        event.elementOptions.appearance = Object.assign(
+            event.elementOptions.appearance,
+            {
+                variables: {
+                    colorPrimary: "#0d6efd",
+                },
+            }
+        );
+    });
 }

9fb63a46d97d0320599527f3ad78c08e50bbdb90
kjmartens <kelsey@solspace.com>
2024-02-29 17:49:30 -0600

    feat(SFT-828): adjustments to Calculation field styles

--- packages/client/src/app/components/form-controls/control-types/calculation-box/calculation-box.editor.styles.ts
+++ packages/client/src/app/components/form-controls/control-types/calculation-box/calculation-box.editor.styles.ts
@@ -15,9 +15,19 @@ export const PreviewTitle = styled.div`
 
 export const CalculationBoxWrapper = styled.div`
   .tagify__input {
-    min-height: 50px;
+    min-height: 80px;
     background-color: #fff;
     line-height: 2.2;
+  }
+
+  .tagify {
+    --tag-bg: ${colors.gray500};
+    --tag-hover: ${colors.gray600};
+    --tag-text-color: ${colors.white};
+    --tags-border-color: ${colors.gray500};
+    --tag-remove-bg: ${colors.red500};
+    --tag-remove-btn-color: ${colors.white};
+    --tag-pad: 0.2em 0.4em;
   }
 
   .sr-only-value {
--- packages/client/src/app/components/form-controls/control-types/calculation-box/calculation-box.help.styles.ts
+++ packages/client/src/app/components/form-controls/control-types/calculation-box/calculation-box.help.styles.ts
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 export const OperatorReferenceTitle = styled.div`
   font-style: italic;
   font-weight: 500;
-  font-size: 15px;
-  color: ${colors.gray500};
+  font-size: 14px;
+  color: ${colors.gray400};
   break-inside: avoid;
 `;
 
@@ -14,15 +14,16 @@ export const OperatorReference = styled.div`
 `;
 
 export const OperatorReferenceItem = styled.div`
+  font-size: 12px;
   break-inside: avoid;
-  color: ${colors.gray400};
-  margin: 0 0 1rem;
+  color: ${colors.gray300};
+  margin: 0 0 0.8rem;
   display: flex;
   flex-direction: column;
   gap: ${spacings.xs};
 
   > span {
-    font-size: 16px;
+    font-size: 14px;
     font-weight: 500;
   }
 `;
@@ -31,10 +32,12 @@ export const Operator = styled.div`
   display: flex;
 
   > mark {
+    font-size: 12px;
+    font-family: 'Courier New', Courier, monospace;
     padding: 0 ${spacings.xs};
-    border-radius: ${borderRadius.lg};
-    background: ${colors.gray200};
-    color: ${colors.gray400};
+    border-radius: ${borderRadius.md};
+    background: ${colors.gray100};
+    color: ${colors.gray500};
     margin-right: ${spacings.md};
     max-height: 20px;
   }

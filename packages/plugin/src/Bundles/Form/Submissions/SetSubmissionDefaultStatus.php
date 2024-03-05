4120303d64f4477ce75b8ceab9cc875c2a364237
Gustavs Gūtmanis <gustavs@solspace.com>
2024-03-05 12:22:29 +0200

    fix(SFT-947): fixing default status not being set

--- packages/plugin/src/Bundles/Form/Submissions/SetSubmissionDefaultStatus.php
+++ packages/plugin/src/Bundles/Form/Submissions/SetSubmissionDefaultStatus.php
@@ -2,31 +2,27 @@
 
 namespace Solspace\Freeform\Bundles\Form\Submissions;
 
-use Solspace\Freeform\Elements\Submission;
 use Solspace\Freeform\Events\Forms\CreateSubmissionEvent;
 use Solspace\Freeform\Form\Form;
 use Solspace\Freeform\Library\Bundles\FeatureBundle;
 use yii\base\Event;
 
-class CreateSubmission extends FeatureBundle
+class SetSubmissionDefaultStatus extends FeatureBundle
 {
     public function __construct()
     {
         Event::on(
             Form::class,
             Form::EVENT_CREATE_SUBMISSION,
-            [$this, 'createSubmission']
+            [$this, 'setStatusId']
         );
     }
 
-    public static function getPriority(): int
-    {
-        return 1;
-    }
-
-    public function createSubmission(CreateSubmissionEvent $event): void
+    public function setStatusId(CreateSubmissionEvent $event): void
     {
         $form = $event->getForm();
-        $form->setSubmission(Submission::create($form));
+        $submission = $event->getSubmission();
+
+        $submission->statusId = $form->getSettings()->getGeneral()->defaultStatus;
     }
 }
--- packages/plugin/src/Events/Forms/CreateSubmissionEvent.php
+++ packages/plugin/src/Events/Forms/CreateSubmissionEvent.php
@@ -2,24 +2,30 @@
 
 namespace Solspace\Freeform\Events\Forms;
 
+use Solspace\Freeform\Elements\Submission;
 use Solspace\Freeform\Events\ArrayableEvent;
 use Solspace\Freeform\Events\FormEventInterface;
 use Solspace\Freeform\Form\Form;
 
 class CreateSubmissionEvent extends ArrayableEvent implements FormEventInterface
 {
-    public function __construct(private Form $form)
+    public function __construct(private Form $form, private Submission $submission)
     {
         parent::__construct();
     }
 
     public function fields(): array
     {
-        return ['form'];
+        return ['form', 'submission'];
     }
 
     public function getForm(): Form
     {
         return $this->form;
+    }
+
+    public function getSubmission(): Submission
+    {
+        return $this->submission;
     }
 }

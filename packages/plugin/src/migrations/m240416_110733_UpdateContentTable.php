8abd765d64bfb8fe9fcc6e7b19d5e5d128b50a85
Gustavs Gūtmanis <gustavs@solspace.com>
2024-04-16 14:46:25 +0300

    fix(SFT-1042): preventing Craft 4 from executing migration

--- packages/plugin/src/migrations/m240416_110733_UpdateContentTable.php
+++ packages/plugin/src/migrations/m240416_110733_UpdateContentTable.php
@@ -2,27 +2,50 @@
 
 namespace Solspace\Freeform\migrations;
 
+use craft\db\Migration;
 use craft\db\Query;
 use craft\migrations\BaseContentRefactorMigration;
 use Solspace\Freeform\Elements\Submission;
 
-class m240416_110733_UpdateContentTable extends BaseContentRefactorMigration
-{
-    public function safeUp(): bool
+if (class_exists(BaseContentRefactorMigration::class, false)) {
+    /**
+     * m240416_110733_UpdateContentTable migration.
+     */
+    class m240416_110733_UpdateContentTable extends BaseContentRefactorMigration
     {
-        // update users
-        $this->updateElements(
-            (new Query())->from(Submission::TABLE),
-            null,
-        );
+        public function safeUp(): bool
+        {
+            // update users
+            $this->updateElements(
+                (new Query())->from(Submission::TABLE),
+                null,
+            );
 
-        return true;
+            return true;
+        }
+
+        public function safeDown(): bool
+        {
+            echo "m240416_110733_UpdateContentTable cannot be reverted.\n";
+
+            return false;
+        }
     }
-
-    public function safeDown(): bool
+} else {
+    class m240416_110733_UpdateContentTable extends Migration
     {
-        echo "m240416_110733_UpdateContentTable cannot be reverted.\n";
+        public function safeUp(): bool
+        {
+            echo "m240416_110733_UpdateContentTable skipped for Craft 4 and less.\n";
 
-        return false;
+            return true;
+        }
+
+        public function safeDown(): bool
+        {
+            echo "m240416_110733_UpdateContentTable cannot be reverted.\n";
+
+            return false;
+        }
     }
 }

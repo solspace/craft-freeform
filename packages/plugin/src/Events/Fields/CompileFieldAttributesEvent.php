f75b23a59f7da5f62c5811cb5038aeff44759903
Kelsey Martens <kelsey@solspace.com>
2024-05-24 03:15:51 -0500

    [SFT-1144][#1277][#1297]: Fixes for Table field overrides, sample formatting templates (#1301)
    * fix(SFT-1144): removing forced overrides for `addButtonLabel` and `removeButtonLabel` on Table fields #1277
    
    * fix(SFT-1144): updating sample formatting template’s CSS to not conflict with other site styles #1297
    
    * fix(SFT-1144): table field attribute collecting
    
    * fix(SFT-1144): adjustments to Table field formatting in Tailwind sample formatting template
    
    ---------
    
    Co-authored-by: Gustavs Gūtmanis <gustavs@solspace.com>

--- packages/plugin/src/Events/Fields/CompileFieldAttributesEvent.php
+++ packages/plugin/src/Events/Fields/CompileFieldAttributesEvent.php
@@ -3,14 +3,22 @@
 namespace Solspace\Freeform\Events\Fields;
 
 use Solspace\Freeform\Fields\FieldInterface;
-use Solspace\Freeform\Library\Attributes\FieldAttributesCollection;
+use Solspace\Freeform\Library\Attributes\Attributes;
 use yii\base\Event;
 
+/**
+ * @template T of Attributes
+ */
 class CompileFieldAttributesEvent extends Event
 {
+    /**
+     * @param T               $attributes
+     * @param class-string<T> $class
+     */
     public function __construct(
         private FieldInterface $field,
-        private FieldAttributesCollection $attributes,
+        private Attributes $attributes,
+        private string $class,
     ) {
         parent::__construct();
     }
@@ -20,15 +28,29 @@ class CompileFieldAttributesEvent extends Event
         return $this->field;
     }
 
-    public function getAttributes(): FieldAttributesCollection
+    /**
+     * @return T
+     */
+    public function getAttributes(): Attributes
     {
         return $this->attributes;
     }
 
-    public function setAttributes(FieldAttributesCollection $attributes): self
+    /**
+     * @param T $attributes
+     */
+    public function setAttributes(Attributes $attributes): self
     {
         $this->attributes = $attributes;
 
         return $this;
+    }
+
+    /**
+     * @return class-string<T>
+     */
+    public function getClass(): string
+    {
+        return $this->class;
     }
 }

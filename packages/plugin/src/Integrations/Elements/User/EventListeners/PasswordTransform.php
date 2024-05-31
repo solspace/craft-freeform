c787e497d9d39e8792530c0ff8a834f2144a7d47
Sean Delaney <sean@solspace.com>
2024-05-31 20:07:47 +0100

    fix(SFT-1135): Suppress Craft's minimum password length if we have our own minimum password length setting set

--- packages/plugin/src/Integrations/Elements/User/EventListeners/PasswordTransform.php
+++ packages/plugin/src/Integrations/Elements/User/EventListeners/PasswordTransform.php
@@ -2,6 +2,9 @@
 
 namespace Solspace\Freeform\Integrations\Elements\User\EventListeners;
 
+use craft\base\Model;
+use craft\elements\User as CraftUser;
+use craft\events\DefineRulesEvent;
 use Solspace\Freeform\Events\Integrations\ElementIntegrations\ProcessValueEvent;
 use Solspace\Freeform\Integrations\Elements\User\User;
 use Solspace\Freeform\Library\Bundles\FeatureBundle;
@@ -9,12 +12,41 @@ use yii\base\Event;
 
 class PasswordTransform extends FeatureBundle
 {
+    private ?int $minLength = null;
+
     public function __construct()
     {
         Event::on(
             User::class,
             User::EVENT_PROCESS_VALUE,
             [$this, 'processPassword']
+        );
+
+        Event::on(
+            CraftUser::class,
+            Model::EVENT_DEFINE_RULES,
+            function (DefineRulesEvent $event) {
+                if (!$this->minLength) {
+                    return;
+                }
+
+                // Remove the `newPassword` rule
+                foreach ($event->rules as $key => $rule) {
+                    if (isset($rule[0]) && $rule[0] === ['newPassword']) {
+                        unset($event->rules[$key]);
+                    }
+                }
+
+                // To add our own `newPassword` rules based on the password field type min length
+                /*
+                $event->rules[] = [
+                    ['newPassword'],
+                    'string',
+                    'min' => $this->minLength,
+                    'tooShort' => \Craft::t('yii', 'Password is too short'),
+                ];
+                */
+            }
         );
     }
 
@@ -26,6 +58,11 @@ class PasswordTransform extends FeatureBundle
 
         if ('newPassword' !== $event->getHandle()) {
             return;
+        }
+
+        $minLength = $event->getFreeformField()->getMinLength();
+        if ($minLength) {
+            $this->minLength = $minLength;
         }
 
         $value = $event->getValue();

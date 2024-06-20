8341373c82b8b42d1cc46115b37e1a159db6a856
Gustavs Gūtmanis <gustavs@solspace.com>
2024-06-20 12:35:18 +0300

    fix(SFT-1185): content panel width in craft 4

--- packages/client/src/app/pages/limited-users/limited-users.tsx
+++ packages/client/src/app/pages/limited-users/limited-users.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Breadcrumb } from '@components/breadcrumbs/breadcrumbs';
 import { EmptyBlock } from '@components/empty-block/empty-block';
 import config, { Edition } from '@config/freeform/freeform.config';
+import classes from '@ff-client/utils/classes';
 import translate from '@ff-client/utils/translations';
 
 import {
@@ -11,11 +12,13 @@ import {
   useLimitedUsersQuery,
 } from './limited-users.queries';
 import { SettingsSidebar } from './limited-users.sidebar';
+import { ContentContainer } from './limited-users.styles';
 
 export const LimitedUsers: React.FC = () => {
   const { data, isFetching } = useLimitedUsersQuery();
   const mutation = useLimitedUsersDeleteMutation();
   const isPro = config.editions.isAtLeast(Edition.Pro);
+  const isCraft5 = config.metadata.craft.is5;
 
   if (!data && isFetching) {
     return <div>Loading...</div>;
@@ -52,7 +55,10 @@ export const LimitedUsers: React.FC = () => {
       <div id="main-content" className="has-sidebar">
         <SettingsSidebar />
 
-        <div id="content-container">
+        <ContentContainer
+          id="content-container"
+          className={classes(!isCraft5 && 'craft-4')}
+        >
           <div id="content" className="content-pane">
             {isPro && (
               <div className="tablepane">
@@ -117,7 +123,7 @@ export const LimitedUsers: React.FC = () => {
               />
             )}
           </div>
-        </div>
+        </ContentContainer>
       </div>
     </div>
   );

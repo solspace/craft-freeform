d95b4b77d3b19e2c48ab96135d00431c45a05c80
kjmartens <kelsey@solspace.com>
2024-06-21 08:28:07 -0500

    fix(SFT-1185): applying same content panel width fix for settings page in craft 4

--- packages/client/src/app/pages/limited-users/limited-users.detail.tsx
+++ packages/client/src/app/pages/limited-users/limited-users.detail.tsx
@@ -5,9 +5,11 @@ import { Breadcrumb } from '@components/breadcrumbs/breadcrumbs';
 import String from '@components/form-controls/control-types/string/string';
 import Textarea from '@components/form-controls/control-types/textarea/textarea';
 import { LoadingText } from '@components/loaders/loading-text/loading-text';
+import config from '@config/freeform/freeform.config';
 import { useSidebarSelect } from '@ff-client/hooks/use-sidebar-select';
 import { PropertyType } from '@ff-client/types/properties';
 import { notifications } from '@ff-client/utils/notifications';
+import classes from '@ff-client/utils/classes';
 import translate from '@ff-client/utils/translations';
 
 import {
@@ -15,7 +17,7 @@ import {
   useLimitedUsersSingleQuery,
 } from './limited-users.queries';
 import { SettingsSidebar } from './limited-users.sidebar';
-import { GroupWrapper, List } from './limited-users.styles';
+import { GroupWrapper, List, ContentContainer } from './limited-users.styles';
 import { ItemBlock } from './limited-users.sub-components';
 import type { Item, RecursiveUpdate } from './limited-users.types';
 
@@ -28,6 +30,7 @@ export const LimitedUsersDetail: React.FC = () => {
   const [description, setDescription] = useState('');
   const [state, setState] = useState([]);
   const mutation = useLimitedUsersMutation(id);
+  const isCraft5 = config.metadata.craft.is5;
 
   useSidebarSelect(5);
 
@@ -120,7 +123,10 @@ export const LimitedUsersDetail: React.FC = () => {
       <div id="main-content" className="has-sidebar">
         <SettingsSidebar />
 
-        <div id="content-container">
+        <ContentContainer
+          id="content-container"
+          className={classes(!isCraft5 && 'craft-4')}
+        >
           <div id="content" className="content-pane" style={{ padding: 0 }}>
             <GroupWrapper>
               <String
@@ -166,7 +172,7 @@ export const LimitedUsersDetail: React.FC = () => {
               </List>
             </GroupWrapper>
           </div>
-        </div>
+        </ContentContainer>
       </div>
     </div>
   );

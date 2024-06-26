229acffbf5a13ba5d6fb3365925b7d3fddd30378
Sean Delaney <sean@solspace.com>
2024-06-26 17:05:48 +0100

    feat(SFT-1217): Added delete modal confirmation screen for forms (#1374)

--- packages/client/src/app/components/modals/modal.types.ts
+++ packages/client/src/app/components/modals/modal.types.ts
@@ -1,7 +1,9 @@
 import type { PropsWithChildren } from 'react';
+import type { GenericValue } from '@ff-client/types/properties';
 
 export type ModalType = React.FC<ModalContainerProps>;
 
 export type ModalContainerProps = PropsWithChildren<{
   closeModal: () => void;
+  data?: GenericValue;
 }>;

3da92d2e3067af2855f065c568f4ca8b8d3517f9
Tim Kelty <tim@craftcms.com>
2024-08-08 14:07:13 -0400

    Don't set noCache headers on every request

--- packages/plugin/src/Bundles/Form/HiddenInputs/CsrfInput.php
+++ packages/plugin/src/Bundles/Form/HiddenInputs/CsrfInput.php
@@ -2,6 +2,7 @@
 
 namespace Solspace\Freeform\Bundles\Form\HiddenInputs;
 
+use craft\helpers\Html;
 use Solspace\Freeform\Events\Forms\OutputAsJsonEvent;
 use Solspace\Freeform\Events\Forms\RenderTagEvent;
 use Solspace\Freeform\Form\Form;
@@ -17,21 +18,13 @@ class CsrfInput extends FeatureBundle
             return;
         }
 
-        if (!\Craft::$app->request->isConsoleRequest) {
-            // Prevent response from being cached with token
-            \Craft::$app->getResponse()->setNoCacheHeaders();
-        }
-
         Event::on(Form::class, Form::EVENT_RENDER_AFTER_OPEN_TAG, [$this, 'attachInput']);
         Event::on(Form::class, Form::EVENT_OUTPUT_AS_JSON, [$this, 'attachToJson']);
     }
 
     public function attachInput(RenderTagEvent $event)
     {
-        $name = \Craft::$app->getConfig()->getGeneral()->csrfTokenName;
-        $token = \Craft::$app->request->csrfToken;
-
-        $event->addChunk('<input type="hidden" name="'.$name.'" value="'.$token.'" />');
+        $event->addChunk(Html::csrfInput());
     }
 
     public function attachToJson(OutputAsJsonEvent $event)

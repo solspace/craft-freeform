1c0e23e26ebb07666feae91917108e19a07e6289
kjmartens <kelsey@solspace.com>
2024-10-01 00:13:18 -0500

    fix(#1529): adding default error message to Regex field type

--- packages/plugin/src/Fields/Implementations/Pro/RegexField.php
+++ packages/plugin/src/Fields/Implementations/Pro/RegexField.php
@@ -24,6 +24,7 @@ class RegexField extends TextField implements ExtraFieldInterface
     #[Input\TextArea(
         label: 'Error Message',
         instructions: "The message a user should receive if an incorrect value is given. It will replace any occurrences of '{{pattern}}' with the supplied regex pattern inside the message if any are found.",
+        value: 'Value is not valid',
     )]
     protected string $message = '';
 

8d6b161c366c20d8ff76da92f51769acf2d84dd8
Gustavs Gūtmanis <gustavs@solspace.com>
2025-02-21 07:39:24 +0200

    checking if the compat checker works

--- packages/plugin/src/Integrations/CRM/HubSpot/Versions/HubSpotV3.php
+++ packages/plugin/src/Integrations/CRM/HubSpot/Versions/HubSpotV3.php
@@ -132,6 +132,13 @@ class HubSpotV3 extends BaseHubSpotIntegration
     {
         $record = $this->getRecord($category);
 
+        $js = '{invalid';
+        if (json_validate($js)) {
+            echo 'Valid JSON';
+        } else {
+            echo 'Invalid JSON';
+        }
+
         $response = $client->get($this->getEndpoint('/properties/'.$record));
         $json = json_decode((string) $response->getBody());
         if (empty($json)) {

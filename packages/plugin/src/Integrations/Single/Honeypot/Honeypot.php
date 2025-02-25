6e0a3ce159b03642e1be7b67d569d10ff3a691e7
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2025-02-25 19:58:02 +0200

    fix(SFT-1830): added setting to allow takeover of inactive user accounts on user registration (#1821)

--- packages/plugin/src/Integrations/Single/Honeypot/Honeypot.php
+++ packages/plugin/src/Integrations/Single/Honeypot/Honeypot.php
@@ -6,6 +6,7 @@ use Solspace\Freeform\Attributes\Integration\Type;
 use Solspace\Freeform\Attributes\Property\Input\Text;
 use Solspace\Freeform\Attributes\Property\Input\TextArea;
 use Solspace\Freeform\Attributes\Property\Middleware;
+use Solspace\Freeform\Attributes\Property\VisibilityFilter;
 use Solspace\Freeform\Library\Integrations\BaseIntegration;
 use Solspace\Freeform\Library\Integrations\EnabledByDefault\EnabledByDefaultTrait;
 use Solspace\Freeform\Library\Integrations\SingletonIntegrationInterface;
@@ -24,6 +25,7 @@ class Honeypot extends BaseIntegration implements SingletonIntegrationInterface
     private const DEFAULT_INPUT_NAME = 'freeform_form_handle';
     private const DEFAULT_MESSAGE = 'Form honeypot is invalid';
 
+    #[VisibilityFilter('enabled')]
     #[Middleware('handle')]
     #[Text(
         label: 'Custom Input Name',
@@ -32,6 +34,7 @@ class Honeypot extends BaseIntegration implements SingletonIntegrationInterface
     )]
     protected string $inputName = '';
 
+    #[VisibilityFilter('enabled')]
     #[TextArea(
         label: 'Custom Error Message',
         instructions: 'Enter a new value to change the default error message for the Freeform Honeypot. This is only applied if the Spam Behavior setting is set to Display Error Messages.',

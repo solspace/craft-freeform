9782b31c1099ac06c96d67655cb6859eb93e31cf
Gustavs Gūtmanis <gustavs@solspace.com>
2025-02-26 10:22:31 +0200

    fix(SFT-1835, #1819): removing container injection

--- packages/plugin/src/Tests/_bootstrap.php
+++ packages/plugin/src/Tests/_bootstrap.php
@@ -3,3 +3,10 @@
 echo "Freeform: Unit Test Suite\n";
 
 require_once __DIR__.'/../../../../vendor/autoload.php';
+
+if (!class_exists(\Craft::class)) {
+    class Craft
+    {
+        public static $container;
+    }
+}

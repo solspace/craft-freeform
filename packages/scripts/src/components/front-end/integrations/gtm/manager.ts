f4d85f2c75f45fd103d1baee5680d3d1343ec8c0
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2025-03-07 21:00:34 +0200

    fix(SFT-1852): loading GTM script as an asset instead of inline for sites with CSP (#1837)

---
+++ packages/scripts/src/components/front-end/integrations/gtm/manager.ts
@@ -0,0 +1,67 @@
+export class GTMManager {
+  private static instance: GTMManager;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private dataLayer: any[];
+  private containerIds: Set<string> = new Set();
+
+  private constructor() {
+    window.dataLayer = window.dataLayer || [];
+    this.dataLayer = window.dataLayer;
+  }
+
+  public static getInstance(): GTMManager {
+    if (!GTMManager.instance) {
+      GTMManager.instance = new GTMManager();
+    }
+
+    return GTMManager.instance;
+  }
+
+  public loadContainer(id: string): void {
+    if (this.containerIds.has(id)) {
+      return;
+    }
+
+    if (this.containerIds.size === 0) {
+      this.loadScript(id);
+    }
+
+    this.dataLayer.push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js',
+      'gtm.container': id,
+    });
+
+    this.containerIds.add(id);
+  }
+
+  private loadScript(id: string): void {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
+
+    const firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+  }
+
+  public observeNewForms(): void {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+          if (node instanceof HTMLFormElement) {
+            const gtmId = node.dataset.gtmId;
+            if (gtmId) {
+              this.loadContainer(gtmId);
+            }
+          }
+        });
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+}

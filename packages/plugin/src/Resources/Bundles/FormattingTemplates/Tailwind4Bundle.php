c5f46829f81b528a1360c8daa03580589737b709
kjmartens <kelsey@solspace.com>
2025-03-17 21:52:08 -0500

    feat(tailwind4): initial commit

---
+++ packages/plugin/src/Resources/Bundles/FormattingTemplates/Tailwind4Bundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Solspace\Freeform\Resources\Bundles\FormattingTemplates;
+
+use craft\web\AssetBundle;
+
+class Tailwind4Bundle extends AssetBundle
+{
+    public function init(): void
+    {
+        $this->sourcePath = '@Solspace/Freeform/templates/_templates/formatting/tailwind-4';
+
+        $this->js = ['_main.js'];
+        $this->css = ['_main.css'];
+
+        parent::init();
+    }
+}

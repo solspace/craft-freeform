0bab4088ed16b56e931ffd821948a06e7ea6d620
thejahid <thejahid@icloud.com>
2025-05-05 13:13:42 +0600

    fix: update scan waiting message to reflect shorter expected duration

--- packages/client/src/app/pages/forms/edit/builder/tabs/form-monitor/results/results.tsx
+++ packages/client/src/app/pages/forms/edit/builder/tabs/form-monitor/results/results.tsx
@@ -301,7 +301,7 @@ export const FMResults: React.FC = () => {
         <NoResults>
           <p>
             {translate(
-              'This form is awaiting its first scan. This could take several hours.'
+              'This form is awaiting its first scan. This could take few minutes.'
             )}
           </p>
         </NoResults>

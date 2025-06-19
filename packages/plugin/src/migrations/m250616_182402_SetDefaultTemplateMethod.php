5f83cc7d2f9514977b7e1eaacc6c7deb27365630
Gustavs Gūtmanis <gustavs@solspace.com>
2025-06-19 11:47:21 +0300

    fix(#2022): prevent a migration from writing to project config if `allowAdminChanges` set to `false`

--- packages/plugin/src/migrations/m250616_182402_SetDefaultTemplateMethod.php
+++ packages/plugin/src/migrations/m250616_182402_SetDefaultTemplateMethod.php
@@ -8,6 +8,10 @@ class m250616_182402_SetDefaultTemplateMethod extends Migration
 {
     public function safeUp(): bool
     {
+        if (!\Craft::$app->config->general->allowAdminChanges) {
+            return true;
+        }
+
         \Craft::$app->projectConfig->set(
             'plugins.freeform.settings.emailTemplateMethod',
             'global'

3750a9de931ed9d9fb5bdd2acbc83b07f1af2e13
Gustavs Gūtmanis <gustavs@solspace.com>
2025-07-07 17:00:14 +0300

    fix(SFT-2218): fixed db notification import

--- packages/plugin/src/Bundles/Backup/Import/FreeformImporter.php
+++ packages/plugin/src/Bundles/Backup/Import/FreeformImporter.php
@@ -537,10 +537,10 @@ class FreeformImporter
         foreach ($collection as $notification) {
             $this->sse->message('info', 'Importing notification: '.$notification->name);
 
-            $record = $notificationsByIdentificator[$notification->id ?? $notification->uid] ?? null;
+            $record = $notificationsByIdentificator[$notification->uid] ?? null;
             if ($record) {
                 if (ImportStrategy::TYPE_SKIP === $strategy) {
-                    $this->notificationTransferIdMap[$notification->id ?? $notification->uid] = $record->id ?? $record->filepath;
+                    $this->notificationTransferIdMap[$notification->uid] = $record->id ?? $record->filepath;
                     $this->sse->message('progress', 1);
 
                     continue;

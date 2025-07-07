b9207a164419cc2c035a2856429ea6e9aae02c8b
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2025-07-07 16:33:37 +0300

    feat(SFT-2206): update styles for lightswitch to match new craft styles (#2058)

--- packages/client/src/utils/comparison.test.ts
+++ packages/client/src/utils/comparison.test.ts
@@ -1,4 +1,4 @@
-import { objectHasAnyKey } from './comparison';
+import { createSemverCompare, objectHasAnyKey } from './comparison';
 
 describe('comparisons', () => {
   describe('objectHasAnyKey', () => {
@@ -10,6 +10,29 @@ describe('comparisons', () => {
     it('should return false if the object does not have any of the specified keys', () => {
       const obj = { a: 1, b: 2, c: 3 };
       expect(objectHasAnyKey(obj, ['d', 'e'])).toBe(false);
+    });
+  });
+
+  describe('createSemverCompare', () => {
+    it('should create a comparison function for the given version', () => {
+      const compare = createSemverCompare('1.0.0');
+      expect(compare('1.0.0')).toBe(true);
+      expect(compare('1.0.1')).toBe(false);
+      expect(compare('0.9.9')).toBe(false);
+    });
+
+    it('should create sub-functions', () => {
+      const compare = createSemverCompare('1.0.0');
+      expect(compare.atLeast('1.0.0')).toBe(true);
+      expect(compare.atMost('1.0.0')).toBe(true);
+
+      expect(compare.below('1.0.0')).toBe(false);
+      expect(compare.above('1.0.0')).toBe(false);
+
+      expect(compare.atLeast('1.0.1')).toBe(false);
+      expect(compare.atMost('1.0.1')).toBe(true);
+      expect(compare.below('1.0.1')).toBe(true);
+      expect(compare.above('1.0.1')).toBe(false);
     });
   });
 });

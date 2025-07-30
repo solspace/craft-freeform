684ed6e13dec8654695e67864e0ae71c4b94a98b
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2025-07-30 22:59:46 +0300

    feat(SFT-2054): add the possibility to set up custom prompt dialog for file upload removal (#2104)

--- packages/plugin/src/Bundles/Digest/DigestBundle.php
+++ packages/plugin/src/Bundles/Digest/DigestBundle.php
@@ -5,17 +5,19 @@ namespace Solspace\Freeform\Bundles\Digest;
 use Carbon\Carbon;
 use craft\helpers\Queue;
 use craft\web\Application;
-use Solspace\Freeform\Bundles\Digest\Providers\DigestLoggerProvider;
 use Solspace\Freeform\Freeform;
 use Solspace\Freeform\Jobs\SendDigestJob;
 use Solspace\Freeform\Library\Bundles\FeatureBundle;
 use Solspace\Freeform\Records\FeedRecord;
-use Solspace\Freeform\Services\Pro\DigestService;
 use yii\base\Event;
 
 class DigestBundle extends FeatureBundle
 {
-    public function __construct(private DigestLoggerProvider $loggerProvider)
+    public const CACHE_KEY_DIGEST = 'freeform-digest-bundle-queued';
+
+    public const CACHE_TTL_DIGEST = 60 * 60; // every 1h
+
+    public function __construct()
     {
         if (!$this->plugin()->isInstalled || \Craft::$app->request->getIsConsoleRequest()) {
             return;
@@ -34,11 +36,9 @@ class DigestBundle extends FeatureBundle
             return;
         }
 
-        if (Freeform::isLockedWithGuard(DigestService::CACHE_KEY_DIGEST, DigestService::LOCK_KEY_DIGEST, DigestService::CACHE_TTL_DIGEST)) {
+        if (Freeform::isLocked(self::CACHE_KEY_DIGEST, self::CACHE_TTL_DIGEST)) {
             return;
         }
-
-        $this->loggerProvider->getLogger()->info('DigestBundle triggerDigest - Started processing');
 
         $settings = $this->plugin()->settings;
 
@@ -46,23 +46,10 @@ class DigestBundle extends FeatureBundle
         $clientRecipients = $settings->getClientDigestRecipients();
 
         if (!$devRecipients->count() && !$clientRecipients->count()) {
-            $this->loggerProvider->getLogger()->info('DigestBundle triggerDigest - Skipped - No recipients', [
-                'devRecipients' => $devRecipients->emailsToArray(),
-                'clientRecipients' => $clientRecipients->emailsToArray(),
-            ]);
-
             return;
         }
 
-        $this->loggerProvider->getLogger()->info('DigestBundle triggerDigest - Adding job to queue', [
-            'devRecipients' => $devRecipients->emailsToArray(),
-            'clientRecipients' => $clientRecipients->emailsToArray(),
-        ]);
-
         $job = new SendDigestJob(new Carbon('now'));
         Queue::push($job, $settings->getQueuePriority());
-
-        $this->loggerProvider->getLogger()->info('DigestBundle triggerDigest - Job added to queue');
-        $this->loggerProvider->getLogger()->info('DigestBundle triggerDigest - Finished processing');
     }
 }

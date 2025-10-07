1f3e99e7f2e77d273be1b3db372e0d292d390b67
Gustavs Gūtmanis <gustavs@solspace.com>
2025-10-07 09:02:17 +0300

    fix(SFT-2410): pass `site` url param to stripe endpoints for non-third-level domain translated sites.

--- packages/scripts/src/components/front-end/payments/stripe/elements.queries.ts
+++ packages/scripts/src/components/front-end/payments/stripe/elements.queries.ts
@@ -20,20 +20,29 @@ const getFormData = (form: HTMLFormElement): FormData => {
 };
 
 const paymentIntents = {
-  create: async (integration: string, form: HTMLFormElement) => {
+  create: async (integration: string, form: HTMLFormElement, site: string) => {
     const formData = getFormData(form);
 
     return ajax.post<ClientSecretResponse>('/freeform/payments/stripe/payment-intents', formData, {
       headers: { 'FF-STRIPE-INTEGRATION': integration },
+      queryParams: new URLSearchParams({ site }),
     });
   },
-  updateAmount: async (integration: string, form: HTMLFormElement, id: string): Promise<UpdateAmountResponse> => {
+  updateAmount: async (
+    integration: string,
+    form: HTMLFormElement,
+    id: string,
+    site: string
+  ): Promise<UpdateAmountResponse> => {
     const formData = getFormData(form);
 
     const { data } = await ajax.post<UpdateAmountResponse>(
       `/freeform/payments/stripe/payment-intents/${id}/amount`,
       formData,
-      { headers: { 'FF-STRIPE-INTEGRATION': integration } }
+      {
+        headers: { 'FF-STRIPE-INTEGRATION': integration },
+        queryParams: new URLSearchParams({ site }),
+      }
     );
 
     return data;
@@ -46,16 +55,20 @@ type UpdateProps = {
   paymentIntentId: string;
   key: string;
   value: string;
+  site: string;
 };
 
 const customers = {
-  update: async ({ integration, form, paymentIntentId }: UpdateProps) => {
+  update: async ({ integration, form, paymentIntentId, site }: UpdateProps) => {
     const formData = getFormData(form);
 
     const { status } = await ajax.post(
       `/freeform/payments/stripe/payment-intents/${paymentIntentId}/customers`,
       formData,
-      { headers: { 'FF-STRIPE-INTEGRATION': integration } }
+      {
+        headers: { 'FF-STRIPE-INTEGRATION': integration },
+        queryParams: new URLSearchParams({ site }),
+      }
     );
 
     return status;

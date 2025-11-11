ea3bc0e7d0c4b718db9239694745b64cfb260116
Gustavs Gūtmanis <gustavs@solspace.com>
2025-11-11 09:33:42 +0200

    fix(SFT-2473): resending notifications not considering site ID

--- packages/plugin/src/Bundles/Notifications/SendListeners/ConditionalNotifications.php
+++ packages/plugin/src/Bundles/Notifications/SendListeners/ConditionalNotifications.php
@@ -88,6 +88,7 @@ class ConditionalNotifications extends NotificationListener
                 new SendNotificationsJob([
                     'formId' => $form->getId(),
                     'submissionId' => $event->getSubmission()->id,
+                    'siteId' => $event->getSiteId(),
                     'postedData' => $postedData,
                     'recipients' => $recipients,
                     'template' => $template,

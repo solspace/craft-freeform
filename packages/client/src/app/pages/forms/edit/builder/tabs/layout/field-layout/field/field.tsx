064dce6e95735fb72d504160718b34114ccfab13
Gustavs Gūtmanis <gustavs@solspace.com>
2025-12-05 11:12:10 +0200

    feat(SFT-2519): field cloning limited users functionality

--- packages/client/src/app/pages/forms/edit/builder/tabs/layout/field-layout/field/field.tsx
+++ packages/client/src/app/pages/forms/edit/builder/tabs/layout/field-layout/field/field.tsx
@@ -2,6 +2,7 @@ import React, { memo, useState } from 'react';
 import { DragPreviewImage } from 'react-dnd';
 import { DuplicateButton } from '@components/elements/duplicate-button/duplicate';
 import { RemoveButton } from '@components/elements/remove-button/remove';
+import config from '@config/freeform/freeform.config';
 import type { Row } from '@editor/builder/types/layout';
 import { useAppDispatch } from '@editor/store';
 import { contextActions } from '@editor/store/slices/context';
@@ -52,6 +53,8 @@ export const Field: React.FC<Props> = memo(
       hoverPosition,
     });
 
+    const canDuplicate = config.limitations.can('layout.fields.clone');
+
     return (
       <>
         <DragPreviewImage
@@ -64,13 +67,15 @@ export const Field: React.FC<Props> = memo(
           ref={drag}
           style={style}
         >
-          <DuplicateButton
-            active={hovering}
-            onClick={() => {
-              dispatch(contextActions.unfocus());
-              dispatch(fieldThunks.duplicate(field, row));
-            }}
-          />
+          {canDuplicate && (
+            <DuplicateButton
+              active={hovering}
+              onClick={() => {
+                dispatch(contextActions.unfocus());
+                dispatch(fieldThunks.duplicate(field, row));
+              }}
+            />
+          )}
           <RemoveButton
             active={hovering}
             onClick={() => {

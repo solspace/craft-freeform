38aca1c6f8164362acb29f9acb00a85aeb9f6a42
Gustavs Gūtmanis <gustavs@solspace.com>
2026-03-17 15:35:56 +0200

    chore: removing url-parse

--- packages/client/src/utils/urls.ts
+++ packages/client/src/utils/urls.ts
@@ -1,15 +1,12 @@
-import parse from "url-parse";
-
 export const baseUrl = window.location.href.replace(/(.*\/freeform).*/i, "$1");
 export const generateUrl = (url?: string, absolute = true): string => {
-  url = (url ?? "")
+  const normalized = (url ?? "")
     .replace(/\/+/g, "/")
     .replace(/^\/(.*)/, "$1")
     .replace(/\/$/, "");
 
-  url = url.length ? `/${url}` : "";
+  const finalPath = normalized.length ? `/${normalized}` : "";
+  const parsed = new URL(`${baseUrl}${finalPath}`);
 
-  const parsedUrl = parse(`${baseUrl}${url}`);
-
-  return absolute ? parsedUrl.href : parsedUrl.pathname;
+  return absolute ? parsed.href : parsed.pathname;
 };

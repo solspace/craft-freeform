c142fe93039f95e394801b7045a7620122b1b06d
Gustavs Gūtmanis <gustavs@solspace.com>
2026-03-19 17:27:13 +0200

    fix: fix a fix

--- packages/plugin/src/Services/ClientAssetsService.php
+++ packages/plugin/src/Services/ClientAssetsService.php
@@ -113,7 +113,7 @@ class ClientAssetsService extends BaseService
 
     private function isDevModeEnabled(): bool
     {
-        return filter_var(App::env(self::ENV_ENABLED), \FILTER_VALIDATE_BOOLEAN);
+        return App::env(self::ENV_ENABLED) ?? false;
     }
 
     private function isDevServerAvailable(): bool

dcef967ca78739043c9bd5d48cff5754c9580510
kjmartens <kelsey@solspace.com>
2026-08-13 16:11:42 -0500

    chore: updating copyright year

--- packages/builder/src/actions/DragDrop.js
+++ packages/builder/src/actions/DragDrop.js
@@ -2,7 +2,7 @@
  * Freeform for Craft CMS
  *
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2025, Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2026, Solspace, Inc.
  * @see           https://docs.solspace.com/craft/freeform
  * @license       https://docs.solspace.com/license-agreement
  */
--- packages/plugin/src/Controllers/SubmitController.php
+++ packages/plugin/src/Controllers/SubmitController.php
@@ -4,7 +4,7 @@
  * Freeform for Craft CMS.
  *
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2025, Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2026, Solspace, Inc.
  *
  * @see           https://docs.solspace.com/craft/freeform
  *
